509169546cbb9d71939f1e34348aaf8d8922c0fd
TheEyeOfAr3s <eherzig@protonmail.com>
2022-08-25 01:45:30 +0200

    v1.1.0

--- src/mod.ts
+++ src/mod.ts
@@ -35,6 +35,28 @@ class Mod implements IPostDBLoadMod
             {
                 mount._props.Slots[0]._props.filters[0].ExcludedFilter = ["57486e672459770abd687134", "5947db3f86f77447880cf76f", "6113d6c3290d254f5e6b27db", "618a5d5852ecee1505530b2a"]
             }
+            if (this.cfg.sightsInMountSlot)
+            {
+                const mods = Object.values(itemsdb).filter((item) => item._props.Slots);
+                mods.forEach((mod) =>
+                {
+                    mod._props.Slots.forEach((slot) =>
+                    {
+                        slot._props.filters.forEach((filter) =>
+                        {
+                            if (filter.Filter.includes("5649a2464bdc2d91118b45a8"))
+                            {
+                                filter.Filter.push("55818add4bdc2d5b648b456f");
+                                filter.Filter.push("55818ad54bdc2ddc698b4569");
+                                filter.Filter.push("5b5f742686f774093e6cb4ff");
+                                filter.Filter.push("55818acf4bdc2dde698b456b");
+                                filter.Filter.push("55818aeb4bdc2ddc698b456a");
+                                filter.ExcludedFilter = ["57486e672459770abd687134", "5947db3f86f77447880cf76f", "6113d6c3290d254f5e6b27db", "618a5d5852ecee1505530b2a", "5a7c74b3e899ef0014332c29", "5d21f59b6dbe99052b54ef83", "5a2c3a9486f774688b05e574", "606f2696f2cb2e02a42aceb1"];
+                            }
+                        })
+                    })
+                })
+            }
         }
     }
 }
